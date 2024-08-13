e0f28def6e80e4e376d196dd1c7e3eabb0b0052b
Rvazquez559 <Rvazquez@gswiring.com>
2024-08-13 15:05:06 -0500

    Update MKS57_CAN.h

--- src/MKS57_CAN.h
+++ src/MKS57_CAN.h
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the CGx-InverseK distribution (https://github.com/Rvazquez559/MKS57D_CAN).
+ * Copyright (c) 2024 Roberto Vazquez.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef MKS57_CAN_H
 #define MKS57_CAN_H
 
